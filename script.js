9b73da0f4d2d9e4b5202aa8c2b113f6a1f9386c5
Gavin Meregillano <98193475+gavmere@users.noreply.github.com>
2025-05-01 18:05:38 -0700

    Filepaths :[

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- Configuration ---
-const pointsDataPath = '/data/data.json';
+const pointsDataPath = 'data/data.json';
 const coordXField = 'umap_x';
 const coordYField = 'umap_y';
 const coordZField = 'umap_z';
